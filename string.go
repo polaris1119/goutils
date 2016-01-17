77b2604766bf7a1e7d7a4acefe1f579b5071d1bd
bower-chosen <notmyemail@bower-chosen.lol>
2016-01-17 20:32:55 +0800

    string utils

---
+++ string.go
@@ -0,0 +1,46 @@
+// Copyright 2016 polaris. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+// http://studygolang.com
+// Author：polaris	polaris@studygolang.com
+
+package goutils
+
+import (
+	"strings"
+	"unicode"
+)
+
+// 驼峰式写法转为下划线写法
+func UnderscoreName(name string) string {
+	buffer := NewBuffer()
+	for i, r := range name {
+		if unicode.IsUpper(r) {
+			if i != 0 {
+				buffer.AppendRune('_')
+			}
+			buffer.AppendRune(unicode.ToLower(r))
+		} else {
+			buffer.AppendRune(r)
+		}
+	}
+
+	return buffer.String()
+}
+
+// 下划线写法转为驼峰写法
+func CamelName(name string) string {
+	name = strings.Replace(name, "_", " ", -1)
+	name = strings.Title(name)
+	return strings.Replace(name, " ", "", -1)
+}
+
+func SearchString(slice []string, s string) int {
+	for i, v := range slice {
+		if s == v {
+			return i
+		}
+	}
+
+	return -1
+}
