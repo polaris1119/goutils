bbce99b639b5a4fbbfbc1e391ca2f38128c93d9f
polaris <polaris@studygolang.com>
2016-03-08 17:32:13 +0800

    must bool

---
+++ bool.go
@@ -0,0 +1,27 @@
+package goutils
+
+import (
+	"log"
+	"strconv"
+)
+
+func MustBool(s string, defaultVal ...bool) bool {
+	getDefault := func() bool {
+		if len(defaultVal) > 0 {
+			return defaultVal[0]
+		}
+		return false
+	}
+
+	if s == "" {
+		return getDefault()
+	}
+
+	b, err := strconv.ParseBool(s)
+	if err != nil {
+		log.Println("goutils MustBool strconv.ParseBool error:", err)
+		return getDefault()
+	}
+
+	return b
+}
